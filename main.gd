93181973484ae71c428405d268b24fbf84a64d50
amono <84484634+alexanderono7@users.noreply.github.com>
2026-04-13 10:15:51 -0700

    modify cell from main.gd

--- main.gd
+++ main.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 func _ready():
-	pass
+	print("bruh!")
+	var width = 4
+	var height = 4
+	self.get_child(0).set_cell(Vector2(3,3),0,Vector2(0,0),0)
+	self.get_child(0).set_cell(Vector2(1,1),0,Vector2(0,0),0)
+	self.get_child(0).set_cell(Vector2(2,2),0,Vector2(0,0),0)
+
 	
-func _process(delta):
+func _process(delta = 0.1):
 	pass
