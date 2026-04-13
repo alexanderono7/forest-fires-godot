8d59566579022fe02b7b86154b2188e666c0d9c6
amono <84484634+alexanderono7@users.noreply.github.com>
2026-04-13 16:46:51 -0700

    ability to read from tilemaplayer

--- main.gd
+++ main.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
+func tilemap():
+	return self.get_child(0)
+
 func _ready():
 	print("bruh!")
-	var width = 4
-	var height = 4
-	self.get_child(0).set_cell(Vector2(3,3),0,Vector2(0,0),0)
-	self.get_child(0).set_cell(Vector2(1,1),0,Vector2(0,0),0)
-	self.get_child(0).set_cell(Vector2(2,2),0,Vector2(0,0),0)
+	var width:int = 3
+	var height:int = 4
+	for i in range(width):
+		for j in range(height):
+			tilemap().set_cell(Vector2(1,1),0,Vector2(0,0),0)
+			print(tilemap().get_cell_tile_data(Vector2(i,j)))
+			#self.get_child(0).set_cell(Vector2(3,3),0,Vector2(0,0),0)
 
 	
 func _process(delta = 0.1):
