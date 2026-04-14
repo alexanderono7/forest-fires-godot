6f6463354092822ec171db9fc3bb2a1dc389e669
amono <84484634+alexanderono7@users.noreply.github.com>
2026-04-14 10:14:22 -0700

    able to get atlas coordinates of tile

--- main.gd
+++ main.gd
@@ -4,15 +4,18 @@ func tilemap():
 	return self.get_child(0)
 
 func _ready():
+	# tree = [5,5]
+	# fire = [8,8]
 	print("bruh!")
 	var width:int = 3
 	var height:int = 4
 	for i in range(width):
 		for j in range(height):
-			tilemap().set_cell(Vector2(1,1),0,Vector2(0,0),0)
+			tilemap().set_cell(Vector2(i,j),0,Vector2(5,5),0)
 			print(tilemap().get_cell_tile_data(Vector2(i,j)))
+			print(tilemap().get_cell_atlas_coords(Vector2(i,j)))
 			#self.get_child(0).set_cell(Vector2(3,3),0,Vector2(0,0),0)
 
 	
-func _process(delta = 0.1):
+func _process(delta):
 	pass
